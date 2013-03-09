7b3cb7ef591d62f89e74cd92441a7f868dc954ed
Ricardo López Salazar <Rash.Pro@gmail.com>
2013-03-08 19:26:49 -0600

    lol

--- config.rb
+++ config.rb
@@ -2,10 +2,10 @@
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_path = "/rashblog/"
+http_images_path = "/rashblog/images"
+http_fonts_path = "/rashblog/fonts"
+css_dir = "public/rashblog/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
